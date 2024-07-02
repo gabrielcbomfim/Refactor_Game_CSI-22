7077eb3a85e864f242f48875c020a5c6f6069720
gabrielcbomfim <gabrielcaetite@hotmail.com>
2024-07-02 19:46:53 -0300

    Reduce lines

--- Gleamshroom.py
+++ Gleamshroom.py
@@ -27,6 +27,8 @@ from game.loadlevel import load_level
 from game import constants
 from game.loadgame import load_img
 from game.loadgame import load_sounds
+from game.displaytext import DisplayText
+from game.displaytext import load_font_img
 
 class Body(Entity):
     def __init__(self, *args):
@@ -105,9 +107,7 @@ for radius in range(1, 250):
 for radius in range(1, 50):
     light_masks_yellow.append(pygame.transform.scale(light_mask_base_yellow, (radius, radius)))
 
-main_font = Font('data/fonts/small_font.png', (255, 255, 255))
-black_font = Font('data/fonts/small_font.png', (0, 0, 1))
-
+main_font, black_font = load_font_img()
 sounds = load_sounds('data/sfx')
 
 global_time = 0
@@ -337,22 +337,7 @@ while True:
         light_surf.blit(dark_surf, (0, 0), special_flags=BLEND_RGBA_MULT)
 
     # tutorial text
-    offset_y = 1 if global_time % 90 > 80 else 0
-    if gd.display_tutorial_text:
-        if gd.current_level == 1:
-            black_font.render('shoot spores with arrow keys', display, (gd.player.center[0] - gd.scroll[0] - main_font.width('shoot spores with arrow keys') // 2 + 1, gd.player.pos[1] - 11 - gd.scroll[1] + offset_y))
-            main_font.render('shoot spores with arrow keys', display, (gd.player.center[0] - gd.scroll[0] - main_font.width('shoot spores with arrow keys') // 2, gd.player.pos[1] - 12 - gd.scroll[1] + offset_y))
-        if (gd.current_level == 2) and (gd.spores_left > 0):
-            black_font.render('destroy red orbs', display, (gd.player.center[0] - gd.scroll[0] - main_font.width('destroy red orbs') // 2 + 1, gd.player.pos[1] - 11 - gd.scroll[1] + offset_y))
-            main_font.render('destroy red orbs', display, (gd.player.center[0] - gd.scroll[0] - main_font.width('destroy red orbs') // 2, gd.player.pos[1] - 12 - gd.scroll[1] + offset_y))
-
-    if gd.spores_left <= 0:
-        black_font.render('out of spores! press "r"', display, (gd.player.center[0] - gd.scroll[0] - main_font.width('out of spores! press "r"') // 2 + 1, gd.player.pos[1] - 11 - gd.scroll[1] + offset_y))
-        main_font.render('out of spores! press "r"', display, (gd.player.center[0] - gd.scroll[0] - main_font.width('out of spores! press "r"') // 2, gd.player.pos[1] - 12 - gd.scroll[1] + offset_y))
-
-    if gd.current_level == 8:
-        black_font.render('thanks for playing!', display, (gd.player.center[0] - gd.scroll[0] - main_font.width('thanks for playing!') // 2 + 1, gd.player.pos[1] - 11 - gd.scroll[1] + offset_y))
-        main_font.render('thanks for playing!', display, (gd.player.center[0] - gd.scroll[0] - main_font.width('thanks for playing!') // 2, gd.player.pos[1] - 12 - gd.scroll[1] + offset_y))
+    DisplayText(gd, global_time, display, main_font, black_font)
 
     # lighting
     display.blit(light_surf, (0, 0), special_flags=BLEND_RGBA_MULT)
