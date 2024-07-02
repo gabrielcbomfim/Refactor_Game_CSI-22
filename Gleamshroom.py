e38401412513dc686a1489b69dc1f76fa38cd13d
Android7001 <m_levipintosousa@hotmail.com>
2024-07-02 18:50:26 -0300

    Correcting previous commit

--- Gleamshroom.py
+++ Gleamshroom.py
@@ -24,22 +24,9 @@ from game.redorb import RedOrb
 from game.glowshroom import GlowShroom
 from game.loadlevel import load_level
 from game import constants
+from game.loadgame import load_img
+from game.loadgame import load_sounds
 
-
-def load_img(path):
-    img = pygame.image.load(path).convert()
-    img.set_colorkey((0, 0, 0))
-    return img
-
-def load_sounds(sound_path):
-    sounds = {sound.split('/')[-1].split('.')[0]: pygame.mixer.Sound(os.path.join(sound_path, sound)) for sound in os.listdir(sound_path)}
-    sounds['thunk'].set_volume(0.2)
-    sounds['shoot'].set_volume(0.3)
-    sounds['bounce'].set_volume(0.8)
-    sounds['transition'].set_volume(0.5)
-    sounds['explode'].set_volume(0.5)
-    sounds['restart'].set_volume(0.5)
-    return sounds
 class Firefly:
     def __init__(self, x, y, theta, w, v, gd, display):
         self.x = x
